b96d3fa449d28924c943e80c2b68e418e3569ff4
Neewlife <dhtpdud2009@gmail.com>
2023-11-07 06:38:11 +0900

    주소검색구현

--- Poten/src/main/java/com/poten/basket/Poten/VO/MapResponse.java
+++ Poten/src/main/java/com/poten/basket/Poten/VO/MapResponse.java
@@ -16,4 +16,7 @@ public class MapResponse {
     private String parkYn;      // 주차 가능여부
     private String createDate;  // 생성 날짜
     private String modifyDate;  // 수정 날짜
+    private String address;          // 주소
+    private String comment;    // 기타 정보란
+    private String courtName;   // 코트 고유명
 }
