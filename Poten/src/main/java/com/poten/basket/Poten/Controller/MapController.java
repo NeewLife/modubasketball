f5decdf8163783dbd12fa0da86a43f2635bb3b03
NeewLife <107593357+NeewLife@users.noreply.github.com>
2023-11-09 19:06:32 +0900

    Update MapController.java

--- Poten/src/main/java/com/poten/basket/Poten/Controller/MapController.java
+++ Poten/src/main/java/com/poten/basket/Poten/Controller/MapController.java
@@ -4,53 +4,54 @@ import com.poten.basket.Poten.Service.MapService;
 import com.poten.basket.Poten.VO.MapRequest;
 import com.poten.basket.Poten.VO.MapResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
 
-@Controller
+@RestController
 public class MapController {
 
     @Autowired
     MapService mapService;
 
     /*
-    * 지도 리스트 조회
-    * return값 - mapList(List 형태의 전체 지도 데이터)
-    * return 지도 데이터 형식 - com.poten.basket.Poten.VO.MapResponse
-    * */
-    @RequestMapping("/")
-    public String openMap(Model model){
-        List<MapResponse> mapList = mapService.mapList();
-        System.out.println("mapList = " + mapList);
-        model.addAttribute("mapList", mapList);
-        return "index";
+     * 지도 리스트 조회
+     * return값 - mapList(List 형태의 전체 지도 데이터)
+     * return 지도 데이터 형식 - com.poten.basket.Poten.VO.MapResponse
+     * */
+    @GetMapping("/")
+    public ResponseEntity<List<MapResponse>> openMap(){
+
+        List<MapResponse> mapList = List<MapResponse>.builder()
+                .data(mapService.mapList())
+                .build();
+        return ResponseEntity.ok(mapList);
     }
 
     /*
-    * 지도 데이터 추가
-    * 필요 params - com.poten.basket.Poten.VO.MapRequest
-    * */
-    @RequestMapping("/spot/create")
-    public String saveSpot(MapRequest params){
+     * 지도 데이터 추가
+     * 필요 params - com.poten.basket.Poten.VO.MapRequest
+     * */
+    @PostMapping("/spot/create")
+    public ResponseEntity saveSpot(@RequestBody MapRequest params){
         System.out.println("====================create====================");
         System.out.println("create의 params = " + params);
         mapService.mapCre(params);
         System.out.println("생성됨");
-        return "redirect:/";
+        return new ResponseEntity(params, HttpStatus.OK);
     }
 
     /*
-    * 지도 데이터 삭제
-    * 필요 params - lat(위도), lon(경도)
-    * */
-    @RequestMapping("/spot/delete")
-    public String deleteSpot(@RequestParam double lat
-                            , @RequestParam double lon){
+     * 지도 데이터 삭제
+     * 필요 params - lat(위도), lon(경도)
+     * */
+    @DeleteMapping("/spot/delete")
+    public ResponseEntity deleteSpot(@RequestParam double lat
+            , @RequestParam double lon){
         System.out.println("====================delete====================");
         System.out.println(lat + "," + lon);
         HashMap<String, Object> params = new HashMap<>();
@@ -58,19 +59,19 @@ public class MapController {
         params.put("lon", lon);
         mapService.mapDel(params);
         System.out.println("삭제됨");
-        return "redirect:/";
+        return new ResponseEntity(params, HttpStatus.OK);
     }
 
     /*
-    * 지도 데이터 수정
-    * 필요 params - com.poten.basket.Poten.VO.MapRequest
-    * */
-    @RequestMapping("/spot/update")
-    public String updateSpot(MapRequest params){
+     * 지도 데이터 수정
+     * 필요 params - com.poten.basket.Poten.VO.MapRequest
+     * */
+    @PutMapping("/spot/update")
+    public ResponseEntity updateSpot(@RequestBody MapRequest params){
         System.out.println("====================update====================");
         System.out.println("update의 params = " + params);
         mapService.mapUpt(params);
-        return "redirect:/";
+        return new ResponseEntity(params, HttpStatus.OK);
     }
 
 }
