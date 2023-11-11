34dda4d48c1bff0249a49fb92009485d84453583
byoungyoon <bur5698@naver.com>
2023-11-11 19:46:50 +0900

    :art: webpack dev, proc 구분

--- Poten/uingx/src/services/common.ts
+++ Poten/uingx/src/services/common.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const webpackMode = process.env.NODE_ENV || 'development';
+
+const baseURL = webpackMode === 'development' ? '/proxy' : '';
+
 export const CustomAxios = axios.create({
-  baseURL: '/proxy',
+  baseURL,
   headers: {
     'Content-type': 'application/json; charset=utf-8',
   },
