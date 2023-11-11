c591851d1d9781c005d7427755ee2901286de1d2
NeewLife <107593357+NeewLife@users.noreply.github.com>
2023-11-11 19:21:41 +0900

    Update MapController.java

--- Poten/src/main/java/com/poten/basket/Poten/Controller/MapController.java
+++ Poten/src/main/java/com/poten/basket/Poten/Controller/MapController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/api")
 public class MapController {
 
   @Autowired
