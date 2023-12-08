6ef904940d7b056bce6312fb0c8753033583c7ff
Neewlife <dhtpdud2009@gmail.com>
2023-12-09 07:56:26 +0900

    카카오 로그인 구현중

---
+++ Poten/src/main/java/com/poten/basket/Poten/Controller/KakaoController.java
@@ -0,0 +1,25 @@
+package com.poten.basket.Poten.Controller;
+
+import com.poten.basket.Poten.Service.KakaoService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kakao")
+@RequiredArgsConstructor
+public class KakaoController {
+
+    private final KakaoService kakaoService;
+
+    @GetMapping("/callback")
+    public ResponseEntity<Object> callback(HttpServletRequest request) throws Exception {
+        KakaoDTO kakaoInfo = kakaoService.getKakaoInfo(request.getParameter("code"));
+
+        return ResponseEntity.ok()
+                .body(kakaoInfo);
+    }
+}
---
+++ Poten/src/main/java/com/poten/basket/Poten/Service/KakaoService.java
@@ -0,0 +1,31 @@
+package com.poten.basket.Poten.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KakaoService {
+    @Value("${kakao.client.id}")
+    private String KAKAO_CLIENT_ID;
+
+    @Value("${kakao.client.secret}")
+    private String KAKAO_CLIENT_SECRET;
+
+    @Value("${kakao.redirect.url}")
+    private String KAKAO_REDIRECT_URL;
+
+    private final static String KAKAO_AUTH_URI = "https://kauth.kakao.com";
+    private final static String KAKAO_API_URI = "https://kapi.kakao.com";
+
+    public String getKakaoLogin() {
+        return KAKAO_AUTH_URI + "/oauth/authorize"
+                + "?client_id=" + KAKAO_CLIENT_ID
+                + "&redirect_uri=" + KAKAO_REDIRECT_URL
+                + "&response_type=code";
+    }
+
+    public KakaoDTO getKakaoInfo(String code) throws Exception{
+        if(code == null) throw new Exception("failed get authorization code");
+
+    }
+}
