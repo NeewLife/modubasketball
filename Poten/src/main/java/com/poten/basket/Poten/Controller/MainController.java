277668c781e1a62d3cb3a43e04d89b85d617a3e6
NeewLife <dhtpdud2009@gmail.com>
2024-02-01 14:37:29 +0900

    서버로 test중

--- Poten/src/main/java/com/poten/basket/Poten/Controller/MainController.java
+++ Poten/src/main/java/com/poten/basket/Poten/Controller/MainController.java
@@ -1,7 +1,13 @@
 package com.poten.basket.Poten.Controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 public class MainController {
@@ -9,5 +15,14 @@ public class MainController {
     @RequestMapping("/")
     public String mainPage(){
         return "index.html";
+    }
+
+    @GetMapping("/login")
+    @ResponseBody
+    public void kakaoLogin(HttpServletResponse response,
+                             @RequestParam String code) throws IOException {
+        System.out.println("code : " + code);
+        String redirect_uri = "https://modubasketball.com/kakao/callback/";
+        response.sendRedirect(redirect_uri + code);
     }
 }
