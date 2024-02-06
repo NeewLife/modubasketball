ecc521322f44a9e79856a5ac0414ddc6b51b321b
NeewLife <dhtpdud2009@gmail.com>
2024-02-06 16:29:24 +0900

    Spring boot router 수정

--- Poten/src/main/java/com/poten/basket/Poten/Controller/MainController.java
+++ Poten/src/main/java/com/poten/basket/Poten/Controller/MainController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class MainController{
-    @GetMapping({ "/", "/map"})
+public class MainController implements ErrorController{
+    @GetMapping({ "/", "/error"})
     public String index() {
         return "index.html";
     }
